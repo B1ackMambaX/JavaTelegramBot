9613750b98e6ca9953dc5cd5fe1e6b7f0823917f
begenFys <nikita.perminov.work@gmail.com>
2023-10-23 15:35:11 +0500

    Add SessionFactoryUtil

--- src/main/java/database/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/database/utils/HibernateSessionFactoryUtil.java
@@ -1,4 +1,32 @@
 package database.utils;
 
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+import database.models.Proglang;
+import database.models.Progquiz;
+import database.models.User;
+
+
 public class HibernateSessionFactoryUtil {
+    private static SessionFactory sessionFactory;
+
+    private HibernateSessionFactoryUtil() {}
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(Proglang.class);
+                configuration.addAnnotatedClass(Progquiz.class);
+                configuration.addAnnotatedClass(User.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            } catch (Exception e) {
+                System.out.println("Exception:" + e);
+            }
+        }
+        return sessionFactory;
+    }
 }
