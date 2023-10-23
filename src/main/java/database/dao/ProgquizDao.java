8f084959011d23368972a37f1b218a8fe5117370
begenFys <nikita.perminov.work@gmail.com>
2023-10-24 00:20:20 +0500

    Add DAO and Service for Progquiz and User Table

--- src/main/java/database/dao/ProgquizDao.java
+++ src/main/java/database/dao/ProgquizDao.java
@@ -1,4 +1,52 @@
 package database.dao;
 
+import database.models.Progquiz;
+import database.models.Progquiz;
+import database.utils.HibernateSessionFactoryUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProgquizDao {
+    public Progquiz findByProgquizId(Integer progquiz_id) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Progquiz progquiz = session.get(Progquiz.class, progquiz_id);
+        session.close();
+        return progquiz;
+    }
+
+    public void save(Progquiz progquiz) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.save(progquiz);
+        tx1.commit();
+        session.close();
+    }
+
+    public void update(Progquiz progquiz) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.update(progquiz);
+        tx1.commit();
+        session.close();
+    }
+
+    public void delete(Progquiz progquiz) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(progquiz);
+        tx1.commit();
+        session.close();
+    }
+
+    public List<Progquiz> findAll() {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        List<Progquiz> progquizs = session.createQuery(
+                "from Progquiz")
+                .getResultList();
+        session.close();
+        return progquizs;
+    }
 }
--- src/main/java/database/dao/UserDao.java
+++ src/main/java/database/dao/UserDao.java
@@ -1,4 +1,50 @@
 package database.dao;
 
+import database.models.User;
+import database.utils.HibernateSessionFactoryUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import java.util.List;
+
 public class UserDao {
+    public User findByUserId(Integer user_id) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        User user = session.get(User.class, user_id);
+        session.close();
+        return user;
+    }
+
+    public void save(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.save(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public void update(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.update(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public void delete(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction tx1 = session.beginTransaction();
+        session.delete(user);
+        tx1.commit();
+        session.close();
+    }
+
+    public List<User> findAll() {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        List<User> users = session.createQuery(
+                        "from User")
+                .getResultList();
+        session.close();
+        return users;
+    }
 }
