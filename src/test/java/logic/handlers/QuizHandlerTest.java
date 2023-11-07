5fdd4701d495262d85db61a0e7113089d0473bb2
B1ackMambaX <b1ackmambax@yandex.ru>
2023-11-07 17:09:29 +0500

    Added test for /stop command

--- src/test/java/logic/handlers/QuizHandlerTest.java
+++ src/test/java/logic/handlers/QuizHandlerTest.java
@@ -6,6 +6,7 @@ import database.models.types.Plathform;
 import database.services.ProglangService;
 import database.services.UserService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
@@ -16,41 +17,65 @@ import java.util.List;
  * Тестирование обработчика квиза
  */
 public class QuizHandlerTest {
+    final List<Progquiz> questions = new ArrayList<>();
+    UserService userService;
+    ProglangService proglangService;
+
+    @BeforeEach
+    void setUp() {
+        questions.add(new Progquiz("Какой метод используется для фильтрации массива?", "filter"));
+        questions.add(new Progquiz("Какое ключевое слово используется для обозначения наследования классов?",
+                "extends"));
+
+        userService = Mockito.mock(UserService.class);
+        proglangService = Mockito.mock(ProglangService.class);
+
+        Mockito.when(proglangService.findProgquizzesByProglangId(1)).thenReturn(questions);
+    }
 
     /**
      * Тестирование различных вариантов ответа
      */
     @Test
-    void answer() {
-        User testUser1 = new User(Plathform.TG, 0L);
-        final List<Progquiz> questions = new ArrayList<>();
-
-        questions.add(new Progquiz("Какой метод используется для фильтрации массива?", "filter"));
-        questions.add(new Progquiz("Какое ключевое слово используется для обозначения наследования классов?",
-                "extends"));
-
-        UserService userService = Mockito.mock(UserService.class);
-        ProglangService proglangService = Mockito.mock(ProglangService.class);
-
-        Mockito.when(proglangService.findProgquizzesByProglangId(1)).thenReturn(questions);
-        Mockito.doNothing().when(userService).update(testUser1);
+    void quizCommandsAndAnswersTest() {
+        User testUser = new User(Plathform.TG, 0L);
+        Mockito.doNothing().when(userService).update(testUser);
 
         QuizHandler quizHandler = new QuizHandler(proglangService , userService, 1);
 
-
-        String quizCommand = quizHandler.getResponse("/quiz", testUser1);
+        String quizCommand = quizHandler.getResponse("/quiz", testUser);
         Assertions.assertEquals(
                 "Тест по ЯП JavaScript, состоит из 2 вопросов\n\nКакой метод используется для фильтрации массива?",
                 quizCommand, "Проверка на команду /quiz");
 
-        String questionRight = quizHandler.getResponse("filter", testUser1);
+        String questionRight = quizHandler.getResponse("filter", testUser);
         Assertions.assertEquals(
                 "Вы ответили правильно!\nКакое ключевое слово используется для обозначения наследования классов?",
                 questionRight, "Проверка на правильный ответ");
 
-        String questionWrong = quizHandler.getResponse("fgdhgfdh", testUser1);
+        String questionWrong = quizHandler.getResponse("fgdhgfdh", testUser);
         Assertions.assertEquals(
                 "Вы ответили неправильно! Правильный ответ:extends\nТест закончен!",
                 questionWrong, "Проверка на неправильный ответ и конец квиза");
+    }
+
+    /**
+     * Тестирование команды /stop
+     */
+    @Test
+    void stopCommand() {
+        User testUser = new User(Plathform.TG, 0L);
+        Mockito.doNothing().when(userService).update(testUser);
+
+        QuizHandler quizHandler = new QuizHandler(proglangService , userService, 1);
+
+        String quizCommand = quizHandler.getResponse("/quiz", testUser);
+        Assertions.assertEquals(
+                "Тест по ЯП JavaScript, состоит из 2 вопросов\n\nКакой метод используется для фильтрации массива?",
+                quizCommand, "Проверка на команду /quiz");
+        String stopCommand = quizHandler.getResponse("/stop", testUser);
+        Assertions.assertEquals(
+                "Тест завершен, чтобы начать заново введите /quiz",
+                stopCommand, "Проверка на команду /stop");
     }
 }
