9aab10cdc7b438d9ddb90ea1c8c8515755d0be25
begenFys <nik220304@gmail.com>
2023-11-08 02:58:19 +0500

    Add logger

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -5,6 +5,8 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.logging.Logger;
+
 
 /**
  * Основной класс, отвечающий за запуск всех сущностей(ботов)
@@ -13,10 +15,13 @@ public class  App
 {
     public static void main( String[] args )
     {
+        Logger logger = Logger.getLogger(App.class.getName());
         try {
+            logger.info("Start App");
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new TelegramBot());
         } catch (TelegramApiException e) {
+            logger.severe(e.toString());
             e.printStackTrace();
         }
     }
