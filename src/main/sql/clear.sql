c1d98a0e22e23833dbd1298021f1cc6b8e640ae2
begenFys <nik220304@gmail.com>
2023-11-28 14:45:48 +0500

    Add table for Quizstate and refactoring code

--- src/main/sql/clear.sql
+++ src/main/sql/clear.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS public.quizstate;
 DROP TABLE IF EXISTS public.progquiz;
 DROP TABLE IF EXISTS public.proglang;
 DROP TABLE IF EXISTS public.user;
--- src/main/sql/init.sql
+++ src/main/sql/init.sql
@@ -1,32 +1,38 @@
 CREATE TABLE IF NOT EXISTS public.proglang (
-    proglang_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     proglang_name VARCHAR(255) UNIQUE NOT NULL
 );
 
 CREATE TYPE public.answer_type AS ENUM ('text', 'code');
 CREATE TABLE IF NOT EXISTS public.progquiz (
-    progquiz_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     proglang_id INTEGER,
     question TEXT NOT NULL,
     answer_type public.answer_type NOT NULL,
     answer_value TEXT NULL,
 
-    FOREIGN KEY (proglang_id) REFERENCES proglang (proglang_id) ON DELETE SET NULL
+    FOREIGN KEY (proglang_id) REFERENCES proglang (id) ON DELETE SET NULL
 );
 
 CREATE TYPE public.plathform AS ENUM ('TG', 'VK');
 CREATE TYPE public.state AS ENUM('QUIZ', 'IDLE');
 CREATE TABLE IF NOT EXISTS public.user (
-    user_id SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     plathform public.plathform NOT NULL,
     plathform_id INTEGER NOT NULL,
     plathform_username VARCHAR(255) NULL,
     plathform_phone VARCHAR(255) NULL,
     plathform_email VARCHAR(255) NULL,
-    "state" public.state NOT NULL,
-    setting_field_1 TEXT NULL,
-    setting_field_2 TEXT NULL,
-    setting_field_3 TEXT NULL,
-    setting_field_4 TEXT NULL,
-    setting_field_5 TEXT NULL
+    "state" public.state NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS public.quizstate(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    current_proglang_id INTEGER,
+    current_question_index INTEGER,
+    current_quiz_stats INTEGER,
+
+
+    FOREIGN KEY (user_id) REFERENCES public.user (id) ON DELETE CASCADE
+)
