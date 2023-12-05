6df6385a5b3055c6242b87096693d60ebfa5ee8b
begenFys <nik220304@gmail.com>
2023-12-05 13:55:43 +0500

    Fix docker-compose

--- src/main/sql/clear.sql
+++ src/main/sql/clear.sql
@@ -2,7 +2,3 @@ DROP TABLE IF EXISTS public.quizstate;
 DROP TABLE IF EXISTS public.progquiz;
 DROP TABLE IF EXISTS public.proglang;
 DROP TABLE IF EXISTS public.user;
-
-DROP TYPE public.answer_type;
-DROP TYPE public.plathform;
-DROP TYPE public.state;
